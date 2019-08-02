49e5f38dd75074715e95bb105cd4df857234f418
Jakob Wedemeyer <jakob@soerensen.com>
2019-08-02 19:06:57 +0200

    :construction: introduces update functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -6,3 +6,7 @@ gccopt() {
   y=${1%.*}
   gcc -Wall -std=c11 $1 -o $y
 }
+
+# update dotfiles
+update_dots() { (cd $HOME/.dotfiles; git fetch master)}
+update_antigen() { (cd $HOME/.dotfiles; curl -L git.io/antigen > "zsh/antigen.zsh")}
