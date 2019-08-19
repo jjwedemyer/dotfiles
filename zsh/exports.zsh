53e8e371a48f79fc72392bd40f956e4274cc7366
Jakob Wedemeyer <jakob@wedemeyer.me>
2019-08-19 12:20:21 +0200

    removing EDITOR subl from exports
    since i didn't really use it
    and it breaks when not loading the ohmyzsh sublime plugin or
    manually setting the alias for Sublime

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,10 +1,4 @@
-# Preferred editor for local and remote sessions
-if [[ -n $SSH_CONNECTION ]]; then
-  export EDITOR='vim'
-else
-  export EDITOR='subl'
-fi
-
+export EDITOR='vim'
 export CHEATCOLORS=true
 
 
