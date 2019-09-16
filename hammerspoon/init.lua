20c9f1ab4cd9b5633132d0e11b9707c96f2949c6
Jakob Wedemeyer <jakob@soerensen.com>
2019-09-16 10:34:17 +0200

    :wrench::construction: updating hammerspoon config
    hammerspoon config now contains a layout for my workdesk setup

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -6,45 +6,43 @@ hs.loadSpoon("WindowHalfsAndThirds")
 spoon.WindowHalfsAndThirds:bindHotkeys(spoon.WindowHalfsAndThirds.defaultHotkeys)
 
 -- keybind shortcuts
-local mash      = {"cmd", "alt", "ctrl"}
+local mash = {"cmd", "alt", "ctrl"}
 local mash_apps = {"cmd", "alt"}
 
-local layout_code = {
-	{
-	  name = {"Terminal"},
-	  func = function(index, win)
-		
-	  end
-	},
-	{
-	  name = {'Code'},
-	  func = function(index, win)
-		 pushWindow(win,0,0,(1/3*2),1)
-	  end
-	},
-	{
-	  name = {'Google Chrome'},
-	  func = function(index, win)
-		 pushWindow(win,0,0,(1/3*2),1)
-	  end
-	}
-  }
+local lgUltrawideWork = "LG ULTRAWIDE"
+local laptopScreen = "Color LCD"
 
-hs.hotkey.bind(mash,"1", function()
-	local win = hs.window.focusedWindow()
-	win:moveToUnit({x=0,y=0, w=0.6, h=1})
+local layout_code_work = {
+    {
+        "Alacritty", nil, lgUltrawideWork, {x = 0.6, y = 0, w = 0.4, h = 1},
+        nil, nil
+    }, {
+        "Google Chrome", nil, lgUltrawideWork, {x = 0, y = 0, w = 0.6, h = 1},
+        nil, nil
+    }, {
+        "Microsoft Outlook", "Posteingang • jakob@soerensen.com",
+        laptopScreen, hs.layout.maximized, nil, nil
+    }
+}
+
+hs.hotkey.bind(mash_apps, "1", function() hs.layout.apply(layout_code_work) end)
+
+hs.hotkey.bind(mash, "1", function()
+    local win = hs.window.focusedWindow()
+    win:moveToUnit({x = 0, y = 0, w = 0.6, h = 1})
 end)
 
-hs.hotkey.bind(mash,"2", function()
-	local win = hs.window.focusedWindow()
-	win:moveToUnit({x=0.6,y=0, w=0.4, h=1})
+hs.hotkey.bind(mash, "2", function()
+    local win = hs.window.focusedWindow()
+    win:moveToUnit({x = 0.6, y = 0, w = 0.4, h = 1})
 end)
 
-hs.hotkey.bind(mash,"W", function()
-	local win = hs.window.focusedWindow()
-  	local f = win:frame()
-	local app = win:application()
-	print(app:title() .. " " .. win:title())	
-	
+hs.hotkey.bind(mash, "W", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local app = win:application()
+    print(app:title() .. " " .. win:title())
+    -- win:maximize()
+
 end)
 
