e290fcda2134f9021e50542106927e6397718fc1
Jakob Wedemeyer <jakob@soerensen.com>
2020-10-02 23:48:30 +0200

    adds zsh functions for .env and git
    adds g function which acts as git alias with a shortcut for git status
    adds envup function to load .env files

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -18,6 +18,27 @@ gccopt() {
   gcc -Wall -std=c11 $1 -o $y
 }
 
+# Load .env file into shell session for environment variables
+
+function envup() {
+  if [ -f .env ]; then
+    export $(sed '/^ *#/ d' .env)
+  else
+    echo 'No .env file found' 1>&2
+    return 1
+  fi
+}
+
+# No arguments: `git status`
+# With arguments: acts like `git`
+g() {
+  if [[ $# -gt 0 ]]; then
+    git "$@"
+  else
+    git status
+  fi
+}
+
 # update dotfiles
 update_dots() { (cd $HOME/.dotfiles; git fetch master)}
 update_antigen() { (cd $HOME/.dotfiles; curl -L git.io/antigen > "zsh/antigen.zsh")}
