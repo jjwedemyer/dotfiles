f12047d693898f3ad59dd65d621f5470173846cd
Jakob Wedemeyer <jakob@soerensen.com>
2020-12-28 22:06:11 +0100

    cleaning some hammerspoon configs, updates to the setup

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -10,12 +10,20 @@ spoon.WindowHalfsAndThirds:bindHotkeys(spoon.WindowHalfsAndThirds.defaultHotkeys
 local mash = {"cmd", "alt", "ctrl"}
 local mash_apps = {"cmd", "alt"}
 local mash_window = {"cmd", "shift"}
+local hyper = {"cmd", "shift", "alt", "ctrl"}
 
+-- work screen config
 local lgUltrawideWork = "LG ULTRAWIDE"
 local laptopScreen = "Color LCD"
 
-local asus4k = "ASUS VP28U"
-local acer = "Acer H233H"
+-- home office screen config
+local dell = "DELL U2720Q"
+local acer = "Acer VG240YU"
+
+local left_two_thirds = {x = 0, y = 0, w = 0.6, h = 1}
+local right_one_third = {x = 0.6, y = 0, w = 0.4, h = 1}
+local large_sized_window = {x = 0.02, y = 0.02, w = 0.96, h = 0.96}
+local smaller_sized_window = {x = 0.1, y = 0.02, w = 0.8, h = 0.96}
 
 local layout_code_work = {
     {
@@ -38,25 +46,36 @@ local layout_code_home = {
 }
 hs.hotkey.bind(mash_window, "1", function()
     local win = hs.window.focusedWindow()
-    win:moveToUnit({x = 0.02, y = 0.02, w = 0.96, h = 0.96})
+    win:moveToUnit(large_sized_window)
 end)
 
 hs.hotkey.bind(mash_window, "2", function()
     local win = hs.window.focusedWindow()
-    win:moveToUnit({x = 0.1, y = 0.02, w = 0.8, h = 0.96})
+    win:moveToUnit(smaller_sized_window)
 end)
 
 hs.hotkey.bind(mash_apps, "1", function() hs.layout.apply(layout_code_work) end)
 hs.hotkey.bind(mash_apps, "2", function() hs.layout.apply(layout_code_home) end)
 
+hs.hotkey.bind(hyper, "Right", function() 
+    local win = hs.window.focusedWindow()
+    win:moveToScreen(dell)
+    win:moveToUnit(large_sized_window)
+end)
+hs.hotkey.bind(hyper, "Left", function() 
+    local win = hs.window.focusedWindow()
+    win:moveToScreen(acer)
+    win:moveToUnit(large_sized_window)
+end)
+
 hs.hotkey.bind(mash, "1", function()
     local win = hs.window.focusedWindow()
-    win:moveToUnit({x = 0, y = 0, w = 0.6, h = 1})
+    win:moveToUnit(left_two_thirds)
 end)
 
 hs.hotkey.bind(mash, "2", function()
     local win = hs.window.focusedWindow()
-    win:moveToUnit({x = 0.6, y = 0, w = 0.4, h = 1})
+    win:moveToUnit(right_one_third)
 end)
 
 hs.hotkey.bind(mash, "W", function()
