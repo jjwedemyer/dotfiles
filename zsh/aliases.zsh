99c3124ea4d266ba137efab7f6b9ca0230aa7e23
Jakob Wedemeyer <jakob@soerensen.com>
2021-01-20 18:53:51 +0100

    some new functions

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -34,5 +34,7 @@ alias duf='du -sh *'
 alias ffd='find . -type d -name'
 alias ff='find . -type f -name'
 
+alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
+
 # Make zsh know about hosts already accessed by SSH
 zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
